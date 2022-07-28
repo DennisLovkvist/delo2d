a940e3c2460f2ad03024682f592cc8375b8db778
Dennis Lövkvist <dennis.lovkvist@protonmail.com>
2022-07-28 15:32:56 +0200

    Fixed jagged edges on textures.

--- delo2d.c
+++ delo2d.c
@@ -128,8 +128,8 @@ void delo2d_load_texture(Texture *texture, char file_path[])
     glGenTextures(1,&texture->renderer_id);
     glBindTexture(GL_TEXTURE_2D,texture->renderer_id);
 
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER,GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER,GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S,GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T,GL_CLAMP_TO_EDGE);
 
