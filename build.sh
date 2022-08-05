0e5a6acf7077722c3b76392276eaee8ccfcb52a0
Dennis Lövkvist <dennis.lovkvist@protonmail.com>
2022-08-05 18:57:26 +0200

    Creates non-existing folders

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+mkdir -p bin/debug/
 cp -u shaders bin/debug/ -d -r
 cp -u textures bin/debug/ -d -r
 gcc -o bin/debug/delo2d_example *.c -Ilibs -lglfw -lGLEW -lGL -msse2 -lm -ldl -w
--- build_optimized.sh
+++ build_optimized.sh
@@ -1,3 +1,4 @@
+mkdir -p bin/release/
 cp -u shaders bin/release/ -d -r
 cp -u textures bin/release/ -d -r
 gcc -o bin/release/delo2d_example *.c -Ilibs -lglfw -lGLEW -lGL -msse2 -lm -ldl -w -O3
