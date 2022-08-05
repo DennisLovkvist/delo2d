cd21a8284272410456bc859d8bcca9200dd31284
Dennis Lövkvist <dennis.lovkvist@protonmail.com>
2022-08-05 13:30:32 +0200

    Changed build scripts for new folder structure

--- build.sh
+++ build.sh
@@ -1,1 +1,4 @@
-gcc stb_image.c main.c delo2d.c  -Ilibs -lglfw -lGLEW -lGL -msse2 -lm -ldl -w; ./a.out
+cp -u shaders bin/debug/ -d -r
+cp -u textures bin/debug/ -d -r
+gcc -o bin/debug/delo2d_example *.c -Ilibs -lglfw -lGLEW -lGL -msse2 -lm -ldl -w
+./bin/debug/delo2d_example
---
+++ build_optimized.sh
@@ -0,0 +1,4 @@
+cp -u shaders bin/release/ -d -r
+cp -u textures bin/release/ -d -r
+gcc -o bin/release/delo2d_example *.c -Ilibs -lglfw -lGLEW -lGL -msse2 -lm -ldl -w -O3
+./bin/release/delo2d_example
