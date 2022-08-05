778a56b4655897d81672bedabeac68924c24eec2
Dennis Lövkvist <dennis.lovkvist@protonmail.com>
2022-08-05 20:04:04 +0200

    Compile specific .c files

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 mkdir -p bin/debug/
 cp -u shaders bin/debug/ -d -r
 cp -u textures bin/debug/ -d -r
-gcc -o bin/debug/delo2d_example *.c -Ilibs -lglfw -lGLEW -lGL -msse2 -lm -ldl -w
+gcc -o bin/debug/delo2d_example stb_image.c delo2d.c main.c game.c -Ilibs -lglfw -lGLEW -lGL -msse2 -lm -ldl -w
 ./bin/debug/delo2d_example
--- build_optimized.sh
+++ build_optimized.sh
@@ -1,5 +1,5 @@
 mkdir -p bin/release/
 cp -u shaders bin/release/ -d -r
 cp -u textures bin/release/ -d -r
-gcc -o bin/release/delo2d_example *.c -Ilibs -lglfw -lGLEW -lGL -msse2 -lm -ldl -w -O3
+gcc -o bin/release/delo2d_example stb_image.c delo2d.c main.c game.c  -Ilibs -lglfw -lGLEW -lGL -msse2 -lm -ldl -w -O3
 ./bin/release/delo2d_example
