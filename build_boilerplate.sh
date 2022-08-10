ab3620bbfb47e89ad3cb7086a504d80a3e474593
Dennis Lövkvist <dennis.lovkvist@protonmail.com>
2022-08-10 03:43:39 +0200

    Gimmie dem warnings!

--- build_boilerplate.sh
+++ build_boilerplate.sh
@@ -1,5 +1,5 @@
 mkdir -p bin/debug/
 cp -u shaders bin/debug/ -d -r
 cp -u textures bin/debug/ -d -r
-gcc -o bin/debug/delo2d_boilerplate stb_image.c delo2d.c boilerplate.c -Ilibs -lglfw -lGLEW -lGL -msse2 -lm -ldl -w
+gcc -o bin/debug/delo2d_boilerplate stb_image.c delo2d.c boilerplate.c -Ilibs -lglfw -lGLEW -lGL -msse2 -lm -ldl -Wall -pedantic-errors
 ./bin/debug/delo2d_boilerplate
