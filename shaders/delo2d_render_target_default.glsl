68699876bbbc8b6d999322a4655aba1276c27ffd
Dennis Lövkvist <dennis.lovkvist@protonmail.com>
2022-08-11 00:42:28 +0200

    changes variable names to match other shaders

--- shaders/delo2d_render_target_default.glsl
+++ shaders/delo2d_render_target_default.glsl
@@ -1,26 +1,26 @@
 #VERT_BEGIN
 #version 330 core
 
-layout (location = 0) in vec2 inPos;
-layout (location = 1) in vec2 inTexCoords;
+layout (location = 0) in vec2 position;
+layout (location = 1) in vec2 tex_coord;
 
-out vec2 texCoords;
+out vec2 v_tex_coord;
 
 void main()
 {
-    gl_Position = vec4(inPos.x, inPos.y, 0.0, 1.0); 
-    texCoords = inTexCoords;
+    gl_Position = vec4(position.x, position.y, 0.0, 1.0); 
+    v_tex_coord = tex_coord;
 }  
 #VERT_END
 #FRAG_BEGIN
 #version 330 core
-out vec4 FragColor;
-in vec2 texCoords;
+out vec4 color;
+in vec2 v_tex_coord;
 
-uniform sampler2D screenTexture;
+uniform sampler2D u_texture;
 
 void main()
 {
-    FragColor = texture(screenTexture,texCoords);    
+    color = texture(u_texture,v_tex_coord);   
 }
 #FRAG_END
