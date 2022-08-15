4d156c389a48c749d7c329c671bc9444ee75e67d
Dennis Lövkvist <dennis.lovkvist@protonmail.com>
2022-08-15 18:15:47 +0200

    Moved example project to separate repo

--- build.sh
+++
@@ -1,5 +0,0 @@
-mkdir -p bin/debug/
-cp -u shaders bin/debug/ -d -r
-cp -u textures bin/debug/ -d -r
-gcc -o bin/debug/delo2d_example stb_image.c delo2d.c main.c game.c -Ilibs -lglfw -lGLEW -lGL -msse2 -lm -ldl -w
-./bin/debug/delo2d_example
--- build_optimized.sh
+++
@@ -1,5 +0,0 @@
-mkdir -p bin/release/
-cp -u shaders bin/release/ -d -r
-cp -u textures bin/release/ -d -r
-gcc -o bin/release/delo2d_example stb_image.c delo2d.c main.c game.c  -Ilibs -lglfw -lGLEW -lGL -msse2 -lm -ldl -w -O3
-./bin/release/delo2d_example
