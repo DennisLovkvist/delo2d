aa749d88d41a5be0e54a8af75ee1235f49a1c020
dennislovkist <dennis.lovkvist@protonmail.com>
2024-05-18 21:23:11 +0200

    changes

--- build_linux.sh
+++ build_linux.sh
@@ -1,3 +1,6 @@
 mkdir -p bin/debug/linux
+cp -u shaders bin/debug/linux -d -r
+cp -u textures bin/debug/linux -d -r
+cp -u fonts bin/debug/linux -d -r
 gcc -o bin/debug/linux/main src/*.c -Ilibs -lfreetype  -Iinclude/ -Iinclude/freetype2 -lglfw -lGLEW -lGL -msse2 -lm -ldl -Wall -pedantic-errors
 ./bin/debug/linux/main
