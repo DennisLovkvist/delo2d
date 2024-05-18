1ea89ccc0e3d2cfae93daa2c705fb0d467e1e1f9
dennislovkist <dennis.lovkvist@protonmail.com>
2024-05-18 21:31:45 +0200

    changes

--- build_boilerplate.sh
+++
@@ -1,6 +0,0 @@
-mkdir -p bin/debug/
-cp -u shaders bin/debug/ -d -r
-cp -u textures bin/debug/ -d -r
-cp -u fonts bin/debug/ -d -r
-gcc -o bin/debug/delo2d_boilerplate *.c -Ilibs -lfreetype  -I/usr/include/freetype2 -lglfw -lGLEW -lGL -msse2 -lm -ldl -Wall -pedantic-errors
-./bin/debug/delo2d_boilerplate
