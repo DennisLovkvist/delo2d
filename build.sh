0205a4b965ea0a6cf4bc212aed44f5536c96e72a
Dennis Lövkvist <dennis.lovkvist@protonmail.com>
2024-11-10 21:21:14 +0100

    imgui converted to header only

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ mkdir -p bin/linux/debug
 cp -u shaders bin/linux/debug -d -r
 cp -u textures bin/linux/debug -d -r
 cp -u fonts bin/linux/debug -d -r
-c_files=("src/main.c" "src/imgui.c")
+c_files=("src/main.c")
 
 gcc -o bin/linux/debug/main "${c_files[@]}" -Ilibs  -Iinclude/ -I/usr/include/freetype2 -lfreetype -lglfw -lGLEW -lGL -lm -ldl;
 ./bin/linux/debug/main
